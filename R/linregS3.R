a26387f60bc279b18ee97c6a6695d54acac9e480
unknown <juapa351@student.liu.se>
2022-09-16 14:40:24 +0200

    - Added ggplot2 to the package in DESCRIPTION - Added @export to the linreg function - Built the first plot as an S3 method

--- R/linregS3.R
+++ R/linregS3.R
@@ -5,6 +5,7 @@
 #'
 #' @param formula Formula or relation between the two model variables.
 #' @param data Data to be used to compute the regression parameters.
+#' @export
 #' @return The list \code{lr_comps} containing the computation results of
 #' the multiple regression model parameters.
 #' @examples
@@ -73,23 +74,56 @@ print.linreg <- function(lr_comps, ...){
       "\nSpeciesvirginica\n", Coefs[3])
 }
 
-plot.linreg <- function(){
+plot.linreg <- function(lr_comps, ...){
+  j <- 1 # sample counter
+  res_acc <- c() # Accumulated residuals (by appendage) for each species
+  median_res <- c() # median of the residuals
+  l <- c()
+  l <- append(l, unique(lr_comps[[2]])) # Fitted values (1 per species)
+  for (i in 1:length(l)){
+    while (isTRUE(lr_comps[[2]][j] == l[i])){
+      res_acc <- append(res_acc, lr_comps[[3]][j]) # Append residuals for each species
+      j <- j + 1 # Next sample
+      
+    }
+    median_res <- append(median_res, median(res_acc)) # Median of residuals for the species
+    res_acc <- c() # Reset accumulateed residuals for the next species
+  }
+  # Create data frames to build correctly the plots 
+  # (one for the points and another one for the median line):
+  datap1 <- data.frame(
+    fit_val = c(lr_comps[[2]]), 
+    res = c(lr_comps[[3]])
+  )
+  datap2 <- data.frame(
+    fit_val = c(l), 
+    res = c(median_res)
+  )
+  
+  # Should import ggplot2 (@import ggplot2 in the function description)
+  p1 <- ggplot2::ggplot( data = datap1) +
+    aes(x = fit_val, y = res) +
+    geom_point() +
+    geom_line(data= datap2, colour='red') +
+    xlab("Fitted values\n lm(Petal.Length ~ Species)") + ylab("Residuals") +
+    ggtitle("Residuals vs Fitted")
+  
+  ## Complete second plot
+}
+
+resid.linreg <- function(lr_comps, ...){
   
 }
 
-resid.linreg <- function(){
+pred.linreg <- function(lr_comps, ...){
   
 }
 
-pred.linreg <- function(){
+coef.linreg <- function(lr_comps, ...){
   
 }
 
-coef.linreg <- function(){
-  
-}
-
-summary.linreg <- function(){
+summary.linreg <- function(lr_comps, ...){
   
 }
 
