338cdb6353e485f3484f44b066a5ea564e985e71
nikostra <niko.stratil@gmail.com>
2022-09-17 18:01:31 +0200

    changed code and tests to RC class, added methods and changed the way everything is exported

--- R/linregRC.R
+++ R/linregRC.R
@@ -1,101 +1,110 @@
-#' Calculate parameters of a multiple regression model.
+#' This is a class representing our model
 #'
-#' This function computes the linear regression parameters and assigns a class
+#' This assigns a RC class
 #' to the returned object with the computation results.
-#'
-#' @param formula Formula or relation between the two model variables.
-#' @param data Data to be used to compute the regression parameters.
-#' @return The list \code{lr_comps} containing the computation results of
-#' the multiple regression model parameters.
-#' @examples
-#' data(iris)
-#' lr_comps <- linreg(Petal.Length ~ Species, data = iris)
+#' @exportClass linreg
+#' @export linreg
 
-
-## Terminal commands to use the function:
-# formulaa = Petal.Length ~ Species
-# # -> explain Petal.Length via Species and Petal.Width
-# data(iris)
-# lr_comps <- linreg(formulaa, data = iris)
-
-## Function definition:
-linreg <- function(formula, data){
-  ## VARIABLE DEFINITION
-  # Independent variable matrix:
-  X <- model.matrix(formula, data)
-  # Dependent variable:
-  y <- data$Petal.Length
-  
-  ## LEAST SQUARES COMPUTATIONS
-  # Regression coefficients:
-  regCoeff <- solve(t(X)%*%X)%*%t(X)%*%y
-  
-  # Fitted values:
-  y_fitted <- X %*%regCoeff
-  
-  # Residuals:
-  residuals <- y-y_fitted
-  
-  # Degrees of freedom:
-  degrees <- nrow(X)-ncol(X)
-  
-  # Residual variance:
-  residual_variance <- (t(residuals)%*%residuals)/degrees
-  
-  # Variance of regression coefficients:
-  coefficient_variance <- residual_variance[1]*solve(t(X)%*%X) 
-  
-  # t-values for each coefficient:
-  t_values <- c()
-  for (i in 1:ncol(coefficient_variance)) {
-    t_values[i] <- regCoeff[i]/sqrt(coefficient_variance[i,i])
-  }
-  
-  # p-values:
-  p_values <- pt(t_values, df=degrees)
-  
-  # Assign class, RC method:
-  linreg <- setRefClass("linreg" ,
-                          fields = list (lr_comps = list(regCoeff, y_fitted, 
-                                                         residuals, degrees, 
-                                                         residual_variance, 
-                                                         coefficient_variance, 
-                                                         t_values, p_values)),
-                        # Defining the computation list straight away like this gives an (S4 OOP) error 
-                          methods = list(
-                            print = function(x){
-                              Coefs <- unique(x[[1]])
-                              cat("Coefficients:\n(Intercept)\n", Coefs[1],
-                                  "\nSpeciesversicolor\n", Coefs[2],
-                                  "\nSpeciesvirginica\n", Coefs[3])
-                            },
-                            plot = function(x){
-                              # Complete
-                            },
-                            resid = function(x){
-                              # Complete
-                            },
-                            pred = function(x){
-                              # Complete
-                            },
-                            coef = function(x){
-                              # Complete
-                            },
-                            summary = function(x){
-                              # Complete
-                            }))
-  ## How to return correctly the class with its objects?
-  
-  # Store the computations:
-  # c <- linreg$new(lr_comps = list(regCoeff, y_fitted, residuals, degrees, 
-  #                                 residual_variance, coefficient_variance, 
-  #                                 t_values, p_values))
-  
-  ## Running the previous commented section and returning c will return only 
-  ## the object, whereas when returning the class linreg we cannot access the
-  ## computations
-
-  return(linreg)
-  
-  #### How to export RC methods in our package????
-}
+linreg <- setRefClass(
+  "linreg" ,
+  fields = list(
+    regCoeff = "matrix",
+    y_fitted = "numeric",
+    residuals = "numeric",
+    degrees = "numeric",
+    residual_variance = "numeric",
+    coefficient_variance = "matrix",
+    t_values = "numeric",
+    p_values = "numeric",
+    formulaString = "character",
+    dataName = "character"
+  ),
+  methods = list(
+    initialize = function(formula, data) {
+      dataName <<- deparse(substitute(data))
+      formulaString <<- deparse(formula)
+      # Independent variable matrix:
+      X <- model.matrix(formula, data)
+      # Dependent variable:
+      y <- data[[all.vars(formula)[1]]]
+      
+      ## LEAST SQUARES COMPUTATIONS
+      # Regression coefficients:
+      regCoeff <<- solve(t(X) %*% X) %*% t(X) %*% y
+      
+      # Fitted values:
+      y_fitted <<- as.vector(X %*% regCoeff)
+      
+      # Residuals:
+      residuals <<- y - y_fitted
+      
+      # Degrees of freedom:
+      degrees <<- nrow(X) - ncol(X)
+      
+      # Residual variance:
+      residual_variance <<- as.vector((t(residuals) %*% residuals) / degrees)
+      
+      # Variance of regression coefficients:
+      coefficient_variance <<- residual_variance[1] * solve(t(X) %*% X)
+      
+      # t-values for each coefficient:
+      t_values <<- c(0)
+      for (i in 1:ncol(coefficient_variance)) {
+        t_values[i]  <<-  regCoeff[i] / sqrt(coefficient_variance[i, i])
+      }
+      
+      # p-values:
+      p_values <<- pt(t_values, df = degrees)
+    },
+    print = function() {
+      writeLines(c(
+        "Call:",
+        paste(
+          "linreg(formula = ",
+          .self$formulaString,
+          ", data = ",
+          .self$dataName,
+          ")"
+        ),
+        "",
+        "Coefficients:"
+      ))
+      cat(rownames(.self$regCoeff))
+      cat("\n")
+      cat(as.vector(.self$regCoeff)) # TODO print properly???
+    },
+    plot = function() {
+      # TODO
+    },
+    resid = function() {
+      return(.self$residuals)
+    },
+    pred = function() {
+      return(.self$y_fitted)
+    },
+    coef = function() {
+      coefs = as.vector(.self$regCoeff)
+      names(coefs) = rownames(.self$regCoeff)
+      return(coefs)
+    },
+    summary = function() {
+      rstderror = paste("Residual Standard Error: ", sqrt(.self$residual_variance),
+                        " on ", degrees," degrees of freedom")
+      writeLines(c(
+          "Call:",
+          paste(
+            "linreg(formula = ",
+            .self$formulaString,
+            ", data = ",
+            .self$dataName,
+            ")"
+          ),
+          "", "Residuals:","Coefficients:",rstderror))
+      
+        # TODO add residuals and coefficient
+        # the issue is that the print() and summary() methods don't work
+        # inside the class because they get overwritten and I don't know how 
+        # to properly display this data (matrix, named vector) with other functions like cat()
+    }
+  )
+)
